20a6f8a3400503bed3e8725eed32d7842846d4e6
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-09-14 21:33:23 +0300

    user model added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql"); //fn
 const { buildSchema } = require("graphql"); //fn
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const EventModel = require("./models/event");
+const UserModel = require("./models/user");
 
 // global vars
 const PORT = process.env.PORT || 3000;
@@ -30,19 +32,32 @@ app.use(
             price: Float!
             date: String!
         }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
         
         input EventInput {
             title: String!
             description: String!
             price: Float!
         }
+
+        input UserInput {
+            email: String!
+            password: String!
+        }
     
         type RootQuery {
             events: [Event!]!
+            users: [User!]!
         }
 
         type RootMutation {
-            createEvents(eInput: EventInput): Event
+            createEvent(eInput: EventInput): Event
+            createUser(uInput: UserInput): User
         }
 
         schema {
@@ -62,16 +77,44 @@ app.use(
             throw e;
           });
       },
-      createEvents: async (args) => {
-        const event = new EventModel({
-          title: args.eInput.title,
-          description: args.eInput.description,
-          price: +args.eInput.price,
-          date: new Date(),
-        });
+      createEvent: async (args) => {
         try {
-          const e = await event.save();
-          return { ...e._doc };
+          const event = new EventModel({
+            title: args.eInput.title,
+            description: args.eInput.description,
+            price: +args.eInput.price,
+            date: new Date(),
+            creator: "5f5fafb4093a6138505f9179",
+          });
+          const user = await UserModel.findById("5f5fafb4093a6138505f9179");
+          if (!user) {
+            throw new Error("No user with this ID");
+          } else {
+            user.userEvents.push(event);
+            await user.save();
+            const e = await event.save();
+            return { ...e._doc };
+          }
+        } catch (e) {
+          throw e;
+        }
+      },
+      createUser: async (args) => {
+        try {
+          const existedUser = await UserModel.findOne({
+            email: args.uInput.email,
+          });
+          if (existedUser) {
+            throw new Error("User with this email already exist");
+          } else {
+            const password = await bcrypt.hash(args.uInput.password, 13);
+            const user = new UserModel({
+              email: args.uInput.email,
+              password,
+            });
+            const e = await user.save();
+            return { ...e._doc, password: null };
+          }
         } catch (e) {
           throw e;
         }
--- models/event.js
+++ models/event.js
@@ -19,6 +19,10 @@ const eventSchema = new Schema({
         type: Date,
         required: true
     },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'UserModel'
+    }
 });
 
 
