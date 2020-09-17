f519870c135f7e127536b05fdb26fb793dc678e9
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-09-17 13:12:39 +0300

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,22 @@ import AuthPage from "./pages/Auth";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
 import "./App.css";
+import MainNavigationBar from "./components/NavigationBar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <React.Fragment>
+        <MainNavigationBar />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
