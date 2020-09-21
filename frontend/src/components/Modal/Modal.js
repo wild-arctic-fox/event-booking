099e0c8d9923535936608254d6e3347d25402ff1
Alyona_Vepritskaya <amusing.arctic.fox@gmail.com>
2020-09-21 16:06:41 +0300

    modal & backdrop added

---
+++ frontend/src/components/Modal/Modal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Modal.css';
+
+const modal = (props) => {
+  return (
+    <div className="modal">
+      <header className="modal__header"><h1>{props.title}</h1></header>
+      <section className="modal__content">
+          {props.children}
+      </section>
+      <section className="modal__actions">
+      {props.canCancel && <button onClick={props.onCancel} className="btn">Cancel</button>}  
+      {props.canConfirm && <button onClick={props.onConfirm} className="btn">Confirm</button>}  
+      </section>
+    </div>
+  );
+};
+
+export default modal;
--- frontend/src/components/pages/Events.js
+++ frontend/src/components/pages/Events.js
@@ -1,8 +1,43 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Modal from "../Modal/Modal";
+import Backdrop from "../Backdrop/Backdrop";
+import "./Events.css";
 
 class EventsPage extends Component {
+  state = {
+    creating: false,
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  cancelModel = () => {
+    this.setState({ creating: false });
+  };
+
+  confirmModel = () => {
+    this.setState({ creating: false });
+    // TODO
+  }
+
   render() {
-    return (<h1>The Events Page</h1>);
+    return (
+      <Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal title="Add Event" canCancel canConfirm onConfirm={this.confirmModel} onCancel={this.cancelModel}>
+            <p>Title</p>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button onClick={this.startCreateEventHandler} className="btn">
+            Create Event
+          </button>
+        </div>
+      </Fragment>
+    );
   }
 }
 
